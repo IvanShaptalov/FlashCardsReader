e36c27cdb78ff4667c09172ead3694ba9294171f
devIS <ivan.shaptalov1@gmail.com>
2023-05-03 17:13:19 +0300

    feat(): updated ui in deleted flash cards

--- lib/views/flashcards/deleted flashcards/deleted_flashcard_collection_widget.dart
+++ lib/views/flashcards/deleted flashcards/deleted_flashcard_collection_widget.dart
@@ -53,58 +53,39 @@ class _DeletedFlashCardCollectionWidgetState
           vertical: SizeConfig.getMediaHeight(context, p: 0.01),
           horizontal: SizeConfig.getMediaWidth(context, p: 0.01)),
       // select items for merge
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black12,
-          ),
-          color: CardViewConfig.defaultCardColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Card(
-          shape: ShapeBorder.lerp(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              0.5),
-          color: getCardColor(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(
-                    top: SizeConfig.getMediaHeight(context, p: 0.03),
-                    bottom: SizeConfig.getMediaHeight(context, p: 0.02)),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: SizeConfig.getMediaHeight(context, p: 0.03),
-                      bottom: SizeConfig.getMediaHeight(context, p: 0.02)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: SizeConfig.getMediaWidth(context, p: 0.02)),
-                    child: Center(
-                      child: Text(
-                        widget.flashCardCollection.title,
-                        style: ConfigFlashCardView.cardTitleTextStyle,
-                        maxLines: 1,
-                      ),
-                    ),
-                  ),
+      child: Card(
+        shape: ShapeBorder.lerp(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            0.5),
+        color: getCardColor(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding:
+                  EdgeInsets.all(SizeConfig.getMediaHeight(context, p: 0.02)),
+              child: Center(
+                child: Text(
+                  widget.flashCardCollection.title,
+                  style: ConfigFlashCardView.cardTitleTextStyle,
+                  maxLines: 1,
                 ),
               ),
-              const Divider(
-                color: Colors.grey,
-                thickness: 1,
-              ),
-              FlashCardCollectionInfo(widget.flashCardCollection),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: getCardActions(isTarget, isSelected, context)),
-            ],
-          ),
+            ),
+            const Divider(
+              color: Colors.grey,
+              thickness: 1,
+            ),
+            FlashCardCollectionInfo(widget.flashCardCollection),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: getCardActions(isTarget, isSelected, context)),
+          ],
         ),
       ),
     );
