321d3aacb7859005b4356bd0242d4c1ac9b3e089
IvanShaptalov <qwr111792@gmail.com>
2023-07-05 14:57:14 +0300

    feat(): cover saved to document directory;

--- lib/model/entities/reader/book_parser/epub.dart
+++ lib/model/entities/reader/book_parser/epub.dart
@@ -1,22 +1,52 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:archive/archive.dart';
 import 'package:flashcards_reader/model/entities/reader/book_model.dart';
 import 'package:flashcards_reader/util/enums.dart';
 import 'package:flashcards_reader/util/error_handler.dart';
 import 'package:flashcards_reader/util/extension_check.dart';
-import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:xml/xml.dart';
 
 class BinderEpub {
   String title = '';
   String author = '';
   String language = '';
-  Image? cover;
+  String coverPath = '';
   ZipDecoder zip = ZipDecoder();
+
+  Future<BookModel> bind(File file) async {
+    // todo bind pdf file
+    String extension = getExtension(file.path);
+    await _unzip(file);
+    BookModel epubBook = BookModel(
+      title: title.isEmpty ? getName(file.path) : title,
+      path: file.path,
+      textSnippet: '',
+      status: BookStatus(
+        reading: false,
+        read: false,
+        wantToRead: false,
+        favourite: false,
+        onPage: 0,
+      ),
+      settings: BookSettings(
+        theme: BookThemes.light,
+      ),
+      file: BookFile(
+        size: file.lengthSync(),
+        extension: extension,
+      ),
+      cover: coverPath,
+      language: language,
+      author: author,
+    );
+    print(epubBook);
+    return epubBook;
+  }
 
   Future<dynamic> _unzip(File file) async {
     final bytes = file.readAsBytesSync();
@@ -25,10 +55,9 @@ class BinderEpub {
     final archive = zip.decodeBytes(bytes);
 
 // Extract the contents of the Zip archive to disk.
-    var opfFile = archive.files
-        .where((p0) => p0.toString().contains('content.opf'))
-        .firstOrNull;
-    debugPrintIt('=================opfFile: $opfFile');
+    var opfFile =
+        archive.files.where((p0) => p0.toString().contains('.opf')).firstOrNull;
+    // debugPrintIt('=================opfFile: $opfFile');
     if (opfFile != null) {
       final document = XmlDocument.parse(utf8.decode(opfFile.content));
       final metadata = document.findAllElements('metadata').firstOrNull;
@@ -47,64 +76,45 @@ class BinderEpub {
           }
         }
 
-        debugPrintIt('=================metadataAttrs: $author');
-        debugPrintIt('=================metadataAttrs: $title');
-        debugPrintIt('=================metadataAttrs: $language');
+        // debugPrintIt('=================author: $author');
+        // debugPrintIt('=================title: $title');
+        // debugPrintIt('=================language: $language');
       } else {
-        print('metadata is null');
+        // print('metadata is null');
+      }
+      // print(document);
 
-        print(document);
-
-        final items = document.findAllElements('item');
-        final coverPath = items
-            .firstWhere((p0) => p0.attributes
-                .where((p0) =>
-                    p0.value == 'image/jpeg' ||
-                    p0.value.toString().contains('cover'))
-                .isNotEmpty)
-            ?.attributes
-            .firstOrNull
-            ?.value;
-        debugPrintIt(coverPath);
-        if (coverPath != null) {
-          var coverFileBytes = archive.files
-              .firstWhere((p0) => p0.toString().contains(coverPath));
-          cover = await getCover(coverFileBytes);
-        }
+      final items = document.findAllElements('item');
+      String? coverPath = items
+          .firstWhere((p0) => p0.attributes
+              .where((p0) =>
+                  p0.value == 'image/jpeg' ||
+                  p0.value.toString().contains('cover'))
+              .isNotEmpty)
+          .attributes
+          .firstOrNull
+          ?.value
+          .toString();
+      // debugPrintIt(coverPath);
+      if (coverPath != null) {
+        var coverFileBytes = archive.files
+            .firstWhere((p0) => p0.toString().contains(coverPath!));
+        coverPath = await _saveCover(coverFileBytes);
       }
     } else {
-      print('opfFile is null');
+      // print('opfFile is null');
     }
   }
 
-  Future<Image> getCover(dynamic fileBytes) async {
-    final codec = await instantiateImageCodec(fileBytes);
-    final frameInfo = await codec.getNextFrame();
-    return Future.value((frameInfo.image) as FutureOr<Image>?);
-  }
-
-  Future<BookModel> bind(File file) async {
-    // todo bind pdf file
-    String extension = getExtension(file.path);
-    await _unzip(file);
-    BookModel fb2Book = BookModel(
-        title: getName(file.path),
-        path: file.path,
-        textSnippet: '',
-        status: BookStatus(
-          reading: false,
-          read: false,
-          wantToRead: false,
-          favourite: false,
-          onPage: 0,
-        ),
-        settings: BookSettings(
-          theme: BookThemes.light,
-        ),
-        file: BookFile(
-          size: file.lengthSync(),
-          extension: extension,
-        ));
-    return fb2Book;
+  Future<String> _saveCover(ArchiveFile fileBytes) async {
+    // print(fileBytes);
+    String ext = getExtension(fileBytes.name);
+    String path =
+        (await getApplicationDocumentsDirectory()).path + uuid.v4() + ext;
+    await File(path).create().then((value) => value
+        .writeAsBytes(fileBytes.content)
+        .then((value) => print('$value saved succesfully')));
+    print(File(path).existsSync());
+    return path;
   }
 }
