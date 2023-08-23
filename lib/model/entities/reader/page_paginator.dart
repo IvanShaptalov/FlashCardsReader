c2f18837aa08a383f7f1517a71f9c2f64f686398
IvanShaptalov <qwr111792@gmail.com>
2023-08-23 10:16:27 +0300

    feat(): added page paginator;

--- lib/model/entities/reader/page_paginator.dart
+++ lib/model/entities/reader/page_paginator.dart
@@ -4,14 +4,16 @@ import 'package:flashcards_reader/views/config/view_config.dart';
 import 'package:flutter/material.dart';
 
 class Page {
-  List<String> words = [];
+  final String text;
+
+  const Page(this.text);
 }
 
 class PagePaginator {
   static BookModel _book = BookModel.asset();
   static set book(book) => _book = book;
   static BookSettings get bookSettings => _book.settings;
-  List<Page> pages = [];
+  static List<Page> pages = [];
   static String bookText = '';
 
   static int setupPages(BuildContext context, double appBarHeigth) {
@@ -30,7 +32,31 @@ class PagePaginator {
         fontSize: $fontSize
         letterSpacing: $letterSpacing
         lineHeigth: $lineHeigth''');
+    double charOnLine = pageWidgth / fontSize;
+    double lineCount = pageHeigth / (lineHeigth * 2 * fontSize);
+    double charOnPage = charOnLine * lineCount;
+    debugPrintIt('''
+        charOnLine: $charOnLine
+        lineCount: $lineCount
+        charOnPage: $charOnPage
+    ''');
+    createPages(charOnPage.round());
     return 10;
+  }
+
+  static int createPages(int charOnPage) {
+    pages = [];
+    int pagesCount = (bookText.length / charOnPage).round();
+    for (int i = 0; i < pagesCount; i++) {
+      if ((i + 1) * charOnPage > bookText.length) {
+        String tmpBookText = bookText.substring(i * charOnPage);
+        pages.add(Page(tmpBookText));
+      } else {
+        String tmpBookText = bookText.substring(i * charOnPage, charOnPage);
+        pages.add(Page(tmpBookText));
+      }
+    }
+    return pagesCount;
   }
 
   static Future<String>? loadBook(BuildContext context) {
