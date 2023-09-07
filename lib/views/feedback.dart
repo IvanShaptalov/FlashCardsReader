4486b6ec2d202e86ddcd27184daee46642cfb950
IvanShaptalov <qwr111792@gmail.com>
2023-09-07 15:15:14 +0300

    feat(feedback): added feedback page

--- lib/views/feedback.dart
+++ lib/views/feedback.dart
@@ -1,7 +1,9 @@
 import 'package:flashcards_reader/views/config/view_config.dart';
 import 'package:flashcards_reader/views/menu/side_menu.dart';
+import 'package:flashcards_reader/views/overlay_notification.dart';
 import 'package:flashcards_reader/views/parent_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // ignore: must_be_immutable
 class FeedBackPage extends ParentStatefulWidget {
@@ -27,7 +29,58 @@ class _MyHomePageState extends ParentState<FeedBackPage> {
       body: Center(
           child: SizedBox(
               height: SizeConfig.getMediaHeight(context),
-              child: const Placeholder())),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () async {
+                        String email =
+                            Uri.encodeComponent("withoutwater792@gmail.com");
+                        String subject =
+                            Uri.encodeComponent("FlashReader Issue");
+                        String body = Uri.encodeComponent("Hi!\n");
+                        print(subject); //output: Hello%20Flutter
+                        Uri mail = Uri.parse(
+                            "mailto:$email?subject=$subject&body=$body");
+                        if (await launchUrl(mail)) {
+                          //email app opened
+                        } else {
+                          OverlayNotificationProvider.showOverlayNotification(
+                              'something went wrong');
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Palette.green300Primary),
+                            color: Palette.green300Primary,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: SizedBox(
+                          width: SizeConfig.getMediaWidth(context, p: 0.6),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'send mail',
+                                  style: FontConfigs.h2TextStyle,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Icon(
+                                  Icons.mail_outline,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ))),
       drawer: SideMenu(appBarHeight),
     ));
 
